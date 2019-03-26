d022d05e34107c74608d602a933b61940dd9cdb2
sakibkabbo <muhtasim.sakib@mail.utoronto.ca>
2019-03-26 18:31:16 -0400

    Can now add rows of OHSlots

--- ohs_gui/src/components/LectureSection.js
+++ ohs_gui/src/components/LectureSection.js
@@ -8,11 +8,14 @@ class LectureSection extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      nrows: 0,
+      rows: [],
       slotNum: 4,
       ohrtable: null
     }
 console.log(this.state.slotNum)
     this.addSlots = this.addSlots.bind(this);
+    this.addRows = this.addRows.bind(this)
 }
 
 addSlots() {
@@ -23,9 +26,18 @@ const newslot = document.createElement("div");
 newslot.className= "book-button";
 trow.appendChild(newslot);
 ohtable.appendChild(trow);*/
+
 const nslotNum = this.state.slotNum + 4;
 this.setState({slotNum : nslotNum});
 console.log(nslotNum);
+}
+
+addRows(){
+const addRows = this.state.rows;
+const nRows = this.state.nrows + 1;
+addRows.push(<OHContainer slotNum={4}/>);
+this.setState({rows : addRows});
+this.setState({nrows : nRows});
 }
 
   render() {
@@ -33,7 +45,7 @@ console.log(nslotNum);
     //const { ohslots } = this.state;
     let params = new URLSearchParams(this.props.location.search);
     const isProf = this.props.user && this.props.user.role === "PROFESSOR";
-    const { slotNum } = this.state;
+    const { slotNum, rows, nrows } = this.state;
     if (isProf){
     return (
     <>
@@ -48,13 +60,12 @@ console.log(nslotNum);
             section_code: {params.get("section_code")}
           </div>
         )}
-          <Button variant="primary" onClick={this.addSlots}>
+          <Button variant="primary" onClick={this.addRows}>
               Add Slots
             </Button>
          <table id="ohrcontainer">
           <tr>
-            <OHContainer slotNum={slotNum}>
-            </OHContainer>
+            {this.state.rows}
            </tr>
          </table>
       </div>
@@ -76,8 +87,7 @@ console.log(nslotNum);
         )}
          <table id="ohrcontainer">
           <tr>
-            <OHContainer slotNum={slotNum}>
-            </OHContainer>
+            {this.state.rows}
            </tr>
          </table>
       </div>
